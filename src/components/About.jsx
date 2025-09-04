2f11f63cef44a0c729d66c12b16ab30f611fa027
Daniyal Hasan <daniyalhasan223@gmail.com>
2025-09-05 03:19:16 +0500

    TruQoin

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,7 @@ const About = () => {
       <div>
         <div>
           <h1 className='mt-8 flex justify-center'>About Us</h1>
-          <p className='flex justify-center items-center'>TrūQoin is a groundbreaking cryptocurrency designed to make</p>
-          financial freedom accessible to everyone. Built on the TRON blockchain, we’ve created a unique system where your activities within the TrūIQ Global ecosystem translate directly into rewards.
+          <p></p>
         </div>
       </div>
     </>
