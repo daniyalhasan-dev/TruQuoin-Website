5df1ce564299b7917629adaa1f3da3f66ec3dc7d
Daniyal Hasan <daniyalhasan223@gmail.com>
2025-09-09 06:52:56 +0500

    TruQoin

--- src/components/Trade.jsx
+++ src/components/Trade.jsx
@@ -118,7 +118,54 @@ const About = () => {
         <div>
           <h1 className="text-white text-3xl text-center pb-5 font-semibold">What We Offer</h1>
         </div>
-      </div>
+
+
+        <div className="md:flex flex-row w-[80vw] mx-auto mb-20">
+          <div className="bg-[#1A1A1A] px-5 py-10 rounded-2xl mx-2 mb-10">
+            <img src="vector1.svg" alt="" />
+            <h1 className="text-2xl text-white font-semibold py-5">Activity-Based <br /> Rewards:</h1>
+            <p className="text-white">Your activities in TruQoin and TruIQ Global drive your success and rewards.  </p>
+          </div>
+          <div className="bg-[#1A1A1A] px-5 py-10 rounded-2xl mx-2 mb-10">
+            <img src="vector1.svg" alt="" />
+            <h1 className="text-2xl text-white font-semibold py-5">Activity-Based <br /> Rewards:</h1>
+            <p className="text-white">Your activities in TruQoin and TruIQ Global drive your success and rewards.  </p>
+          </div>
+          <div className="bg-[#1A1A1A] px-5 py-10 rounded-2xl mx-2 mb-10">
+            <img src="vector1.svg" alt="" />
+            <h1 className="text-2xl text-white font-semibold py-5">Activity-Based <br /> Rewards:</h1>
+            <p className="text-white">Your activities in TruQoin and TruIQ Global drive your success and rewards.  </p>
+          </div>
+        </div>
+
+        <div className="w-[80vw] mx-auto ">
+          <div className="bg-[#EE5A02] rounded-3xl py-10 text-center">
+            <h1 className="text-5xl text-white pb-10">Our People</h1>
+            <p className="text-white sm:px-37 px-5">The real power of cryptocurrency and digital assets is the activity of the people engaged in its creation and use. From miners to users, product creators to markers, TrūQoin harnesses the energy from everyone involved and creates a stronger, more stable and valuable project.</p>
+            <button className="px-5 py-2 rounded-lg border border-white text-white hover:text-black duration-300 mt-8">
+          Launch WebApp
+        </button>
+          </div>
+        </div>
+
+          <div className="md:flex flex-row w-[70vw] mx-auto text-center mt-25 mb-10">
+          <div className="bg-[#1A1A1A] sm:px-30 py-10 rounded-2xl mx-2 mb-10">
+            <img src="vector1.svg" alt="" className="mx-auto"/>
+            <h1 className="text-2xl text-white font-semibold py-5">Address 1:</h1>
+            <p className="text-white">TrūQoin Headquarters 123 Blockchain Drive, Tech City, USA</p>
+          </div>
+          <div className="bg-[#1A1A1A] sm:px-30 py-10 rounded-2xl mx-2 mb-10">
+            <img src="vector1.svg" alt="" className='mx-auto'/>
+            <h1 className="text-2xl text-white font-semibold py-5">Address 1:</h1>
+            <p className="text-white">TrūQoin Headquarters 123 Blockchain Drive, Tech City, USA</p>
+          </div>
+          </div>
+
+          <div className="text-center">
+            <h1 className="text-3xl text-white pb-5">Follow us</h1>
+            <p className="text-white">Be Part of our TruQoin Community</p>
+          </div>
+        </div>
     </>
   );
 };
